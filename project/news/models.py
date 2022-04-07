1d6814467998116bfb216f55d515dc2d6eef56d3
Ali_Uik <toxic_m@mail.ru>
2022-04-07 11:48:01 +0500

    add_news.html,view_news.html,forms.py

--- project/news/models.py
+++ project/news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class News(models.Model):
@@ -12,6 +13,9 @@ class News(models.Model):
     # rasmlarni photos papkasiga saqlaydi. photos/%Y/%m/%d/ - yillar/oylar/kunlar buyicha papkaga ajratib saqlaydi
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={'news_id': self.pk})
 
     def my_func(self):
         return 'Hello from models'
@@ -27,6 +31,9 @@ class News(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'category_id': self.pk})
 
     def __str__(self):
         return self.title
